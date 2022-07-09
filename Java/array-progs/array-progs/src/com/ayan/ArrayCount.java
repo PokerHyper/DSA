bc0fb9247651f2ba11e7fba6f765995160942f36
PokerHyper <deyayan77@gmail.com>
2022-07-09 21:15:16 +0530

    up to dating

--- Java/array-progs/array-progs/src/com/ayan/ArrayCount.java
+++ Java/array-progs/array-progs/src/com/ayan/ArrayCount.java
@@ -1,2 +1,12 @@
-package com.ayan;public class ArrayCount {
+package com.ayan;
+
+import java.util.Scanner;
+
+public class ArrayCount {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = {1, 2, 3, 55, 66};
+        int n = arr.length;
+        System.out.println(n);
+    }
 }
--- Java/array-progs/array-progs/src/com/ayan/HillsValleys.java
+++ Java/array-progs/array-progs/src/com/ayan/HillsValleys.java
@@ -1,2 +1,29 @@
-package com.ayan;public class HillsValleys {
+package com.ayan;
+
+public class HillsValleys {
+    public static void main(String[] args) {
+        int[] arr = {2,4,1,1,6,5};
+        int count = countHillValley(arr);
+        System.out.println(count);
+    }
+    static int countHillValley(int[] nums) {
+        int n = nums.length;
+        int count=0;
+        for(int i =1; i < n -1; i++){
+            int j = i + 1;
+            while(j < n && nums[i] == nums[j]){
+                j ++;
+            }
+            if(j < n && (nums[i] > nums[j] && nums[i] > nums[i-1])){
+                count++;
+            }
+            if(j < n && (nums[i] < nums[j] && nums[i] < nums[i-1])){
+                count++;
+            }
+            i = j -1;
+
+        }
+        return count;
+
+    }
 }
